a99ff9d17a894237d9905465a873f2f5287a3bcb
Bhavya Hegde <85700971+hegdebhavya@users.noreply.github.com>
2023-12-08 14:12:49 -0800

    initial commit

---
+++ backend/ipgeomapper/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+#from .utils import geolocate_ip_address 
